e123ce6ddb74232bd8cbc7f2783ef158b75ac723
Shridhar Govindaiah <vc.shridhar.govindaiah@lowes.com>
2021-05-07 18:21:59 +0530

    Added Pagination and Filtering. Removed hardcoded data

--- src/exportTable/AgGridTable.js
+++ src/exportTable/AgGridTable.js
@@ -5,25 +5,23 @@ import "ag-grid-enterprise";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-import data from "./rowData.json";
-
 const AgGridTable = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
-  const [rowData, setRowData] = useState(data);
+  const [rowData, setRowData] = useState([]);
 
   const onGridReady = (params) => {
     setGridApi(params.api);
     setGridColumnApi(params.columnApi);
     //setRowData(data);
 
-    // const updateData = (data) => {
-    //   setRowData(data);
-    // };
+    const updateData = (data) => {
+      setRowData(data);
+    };
 
-    // fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
-    //   .then((resp) => resp.json())
-    //   .then((data) => updateData(data));
+    fetch("https://www.ag-grid.com/example-assets/small-olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
   };
 
   const onBtExportExcel = () => {
@@ -34,13 +32,33 @@ const AgGridTable = () => {
     gridApi.exportDataAsCsv();
   };
 
+  const onPageSizeChanged = (newPageSize) => {
+    var value = document.getElementById("page-size").value;
+    gridApi.paginationSetPageSize(Number(value));
+  };
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <div className="container">
-        <div>
+        <div style={{ display: "flex", spacing: "20px" }}>
+          <div style={{ margin: "5px" }}>
+            Page Size:
+            <select onChange={() => onPageSizeChanged()} id="page-size">
+              <option value="10" selected={true}>
+                10
+              </option>
+              <option value="100">100</option>
+              <option value="500">500</option>
+              <option value="1000">1000</option>
+            </select>
+          </div>
           <button
             onClick={() => onBtExportExcel()}
-            style={{ marginBottom: "5px", fontWeight: "bold" }}
+            style={{
+              marginBottom: "5px",
+              fontWeight: "bold",
+              marginRight: "5px",
+            }}
           >
             Export to Excel
           </button>
@@ -55,7 +73,7 @@ const AgGridTable = () => {
           <div
             id="myGrid"
             style={{
-              height: 900,
+              height: "100%",
               width: "100%",
             }}
             className="ag-theme-alpine"
@@ -63,24 +81,39 @@ const AgGridTable = () => {
             <AgGridReact
               defaultColDef={{
                 sortable: true,
-                filter: true,
+                floatingFilter: true,
                 resizable: true,
                 minWidth: 100,
                 flex: 1,
               }}
               onGridReady={onGridReady}
-              rowData={data}
+              pagination={true}
+              paginationPageSize={10}
+              rowData={rowData}
+              domLayout={"autoHeight"}
             >
               <AgGridColumn headerName="Group A">
-                <AgGridColumn field="athlete" minWidth={200} />
-                <AgGridColumn field="country" minWidth={200} />
+                <AgGridColumn
+                  field="athlete"
+                  filter="agSetColumnFilter"
+                  filterParams={{ applyMiniFilterWhileTyping: true }}
+                />
+                <AgGridColumn
+                  field="country"
+                  filter="agSetColumnFilter"
+                  filterParams={{ applyMiniFilterWhileTyping: true }}
+                />
               </AgGridColumn>
               <AgGridColumn headerName="Group B">
-                <AgGridColumn field="sport" minWidth={150} />
-                <AgGridColumn field="gold" />
-                <AgGridColumn field="silver" />
-                <AgGridColumn field="bronze" />
-                <AgGridColumn field="total" />
+                <AgGridColumn
+                  field="sport"
+                  filter="agSetColumnFilter"
+                  filterParams={{ applyMiniFilterWhileTyping: true }}
+                />
+                <AgGridColumn field="gold" filter="agNumberColumnFilter" />
+                <AgGridColumn field="silver" filter="agNumberColumnFilter" />
+                <AgGridColumn field="bronze" filter="agNumberColumnFilter" />
+                <AgGridColumn field="total" filter="agNumberColumnFilter" />
               </AgGridColumn>
             </AgGridReact>
           </div>
