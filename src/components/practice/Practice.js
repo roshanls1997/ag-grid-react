9a80e819007f8f322825133467972acb30c86294
Roshan lal Sahu <rosahu@lowes.com>
2021-05-07 16:59:43 +0530

    added grouping, checkbox, pivot

--- src/components/practice/Practice.js
+++ src/components/practice/Practice.js
@@ -2,30 +2,76 @@ import React, { useEffect, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import './Practice.scss';
 import 'ag-grid-enterprise';
+import { Button } from 'semantic-ui-react';
 
 
 const Practice = () => {
 
 	const [tableData, setTableData ] = useState();
 	useEffect(() => {
-		fetch('https://www.ag-grid.com/example-assets/small-row-data.json')
+		fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
 			.then(resp => resp.json())
 			.then(data => setTableData(data))
-			.catch(error => alert('some error occured: ', error));
+			.catch(error =>
+				 alert('some error occured: ', error));
 	}, []);
+
+	const [ gridApi, setGridApi ] = useState();
+	const [ columnApi, setColumnApi ] = useState();
+	const handleGridReady = param => {
+		setGridApi(param.api);
+		setColumnApi(param.columnApi)
+	}
+
+	const defaultColDef = {
+		sortable: true,
+		filter: 'agTextColumnFilter',
+		resizable: true,
+		floatingFilter: true,
+		width: 300,
+		enableRowGroup: true,
+		enablePivot: true,
+		enableValue: true,
+		editable: true
+	}
+
+	const handleAction = (event, data) => {
+		event.preventDefault();
+		console.log(data);
+	}
 
 	const columns = [
 		{
-			headerName: 'Make',
-			field: 'make'
+			headerName: 'Country',
+			field: 'country',
+			filter: true,
+			headerTooltip: 'this is the header tooltip'
 		},
 		{
-			headerName: 'Model',
-			field: 'model'
+			headerName: 'Year',
+			field: 'year'
 		},
 		{
-			headerName: 'Price',
-			field: 'price'
+			headerName: 'Athelete',
+			field: 'athlete'
+		},
+		{
+			headerName: 'Total',
+			field: 'total',
+			aggFunc: 'sum'
+		},
+		{
+			headerName: 'Actions',
+			field: 'actions',
+			rowGroup: false,
+			cellRendererFramework: params => {
+				return (
+					params.data === undefined ? '' : <div>
+						<Button primary size="tiny" onClick={(e) => handleAction(e, params)}>some action</Button>
+						<a target="_blank" href="https://www.ag-grid.com/react-grid/getting-started/" rel="noreferrer">ag grid docs</a>
+					</div>
+				)
+			}
 		}
 	]
 
@@ -33,10 +79,25 @@ const Practice = () => {
 		<div className="practice-page">
 			<section className="ag-grid-table-section">
 				<h2>AG Grid Table</h2>
-				<div className="ag-theme-alpine" style={{width: '100%', height: '600px'}}>
+				<div className="ag-theme-material" style={{height: '600px'}}>
 				<AgGridReact
 					rowData={tableData}
 					columnDefs={columns}
+					defaultColDef={defaultColDef}
+					rowSelection="multiple"
+					onGridReady={params => handleGridReady(params)}
+					groupSelectsChildren={true}
+					autoGroupColumnDef={{
+						headerName: "custom group header",
+						field: 'country',
+						cellRendererParams: {
+							checkbox: true
+						}
+					}}
+					rowGroupPanelShow='always'
+					pivotPanelShow='always'
+					sideBar={{toolPanels: ['columns', 'filters']}}
+					pagination={true}
 				/>
 				</div>
 			</section>
